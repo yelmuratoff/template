389c64912f7b55ee4d6496ef2b1fbde1b0b66f37
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-09-14 23:10:27 +0500

    ! r updated operate script

--- automation/operate.mk
+++ automation/operate.mk
@@ -22,14 +22,8 @@ setup:
 	@fvm dart run build_runner build --delete-conflicting-outputs
 
 setup-ios:
-	@cd ios
-	@pod deintegrate
-	@pod repo update
-	@pod update
-	@cd ..
+	@cd ios && pod deintegrate && pod repo update && pod update && cd ..
 	@fvm flutter clean
 	@fvm flutter pub get
 	@fvm dart run build_runner build --delete-conflicting-outputs
-	@cd ios
-	@pod install
-	@cd ..
+	@cd ios && pod install && cd ..

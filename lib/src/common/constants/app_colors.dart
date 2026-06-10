e5e50d7c166ecb1645ac3d2ef95d4d7406931891
yelmuratoff <yelamanyelmuratov@gmail.com>
2026-06-10 22:45:22 +0500

    refactor: remove unused utility files and update theme storage logic

--- lib/src/common/constants/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// `AppColors` is a class that holds all the `static` colors used in the app.
-final class AppColors {
-  static const primary = Colors.blue;
-}
